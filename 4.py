6c2281585e91e5435c08f933299cb3a4e68922dc
hahalima <chrishalima@gmail.com>
2015-06-27 23:02:19 -0400

    edited #4
    #4.py

--- 4.py
+++ 4.py
@@ -2,7 +2,7 @@ def tupleList():
 	# get input from the user
 	input = raw_input()
 	# use the split() method to make the string to list
-	inputList = input.split()
+	inputList = input.split(',')
 	# convert the list into a tuple
 	inputTuple = tuple(inputList)
 	print inputList
